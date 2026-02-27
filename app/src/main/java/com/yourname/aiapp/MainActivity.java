a0d6614116982392e412fedc7762a080a08001bd
yashwantbarache44-max <yashwantbarache44@gmail.com>
2026-02-27 21:43:35 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/yourname/aiapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yourname.aiapp;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("YOUR_AI_STUDIO_URL"); 
+        setContentView(webView);
+    }
+}
